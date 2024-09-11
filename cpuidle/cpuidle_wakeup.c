794b634f211251ac804fa215e489fa5a7c6747b9
Aboorva Devarajan <aboorvad@linux.ibm.com>
2024-09-11 15:42:34 -0500

    cpuidle_wakeup: add test duration

--- cpuidle/cpuidle_wakeup.c
+++ cpuidle/cpuidle_wakeup.c
@@ -1,20 +1,31 @@
+#define _GNU_SOURCE
+
+#include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <signal.h>
+#include <assert.h>
+#include <sys/syscall.h>
 #include <dirent.h>
-#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #define MAX_IDLE_STATES 10
 #define DEFAULT_WAKEUP_INTERVAL_NS 100000ULL
 #define DEFAULT_TEST_DURATION_SEC 5
+
+unsigned int stop = 0;
 
 typedef enum { PIPE_WAKEUP, TIMER_WAKEUP } wakeup_mode_t;
 typedef enum { IDLE_STATE_NUMERIC, IDLE_STATE_STRING } idle_state_type_t;
 
 static wakeup_mode_t current_wakeup_mode = TIMER_WAKEUP;
 
-int wakee_cpu_id = 0;
-int waker_cpu_id = 0;
+int wakee_cpu_id = 0, waker_cpu_id = 0;
+static pthread_t wakee_tid, waker_tid;
+
 int total_idle_states = 0;
 
 unsigned long wakeup_interval_ns = DEFAULT_WAKEUP_INTERVAL_NS;
@@ -39,7 +50,6 @@ struct idle_state {
 };
 
 struct idle_state wakee_idle_states[MAX_IDLE_STATES];
-
 
 void get_cpu_idle_state(const char *field, int cpu, int state, void *value, idle_state_type_t type) {
     char filepath[256];
@@ -101,6 +111,77 @@ static void initialize_wakee_idle_states(void) {
     }
 }
 
+static cpu_set_t* initialize_cpuset(int num_cpus, int cpu_id) {
+    cpu_set_t *cpuset = CPU_ALLOC(num_cpus);
+    if (cpuset == NULL) {
+        perror("Unable to allocate cpuset");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t size = CPU_ALLOC_SIZE(num_cpus);
+    CPU_ZERO_S(size, cpuset);
+    CPU_SET_S(cpu_id, size, cpuset);
+
+    return cpuset;
+}
+
+static void set_thread_affinity(pthread_attr_t *attr, cpu_set_t *cpuset) {
+    size_t size = CPU_ALLOC_SIZE(sysconf(_SC_NPROCESSORS_ONLN));
+    if (pthread_attr_setaffinity_np(attr, size, cpuset)) {
+        perror("Error setting thread affinity");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+static void *waker_fn(void *arg) {
+    while (!stop) {
+
+    }
+}
+
+static void *wakee_fn(void *arg) {
+    while (!stop) {
+
+    }
+}
+
+static void create_thread(pthread_t *thread, pthread_attr_t *attr, void *(*start_routine)(void *), const char *thread_name) {
+    int ret = pthread_create(thread, attr, start_routine, NULL);
+    if (ret) {
+        fprintf(stderr, "Failed to create %s thread\n", thread_name);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void initialize_threads(void) {
+    int num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+
+    pthread_attr_t wakee_attr, waker_attr;
+    pthread_attr_init(&wakee_attr);
+    pthread_attr_init(&waker_attr);
+
+    printf("CPU Wakee: %d, CPU Waker: %d\n", wakee_cpu_id, waker_cpu_id);
+
+    cpu_set_t *cpuset_wakee = initialize_cpuset(num_cpus, wakee_cpu_id);
+    cpu_set_t *cpuset_waker = initialize_cpuset(num_cpus, waker_cpu_id);
+
+    set_thread_affinity(&wakee_attr, cpuset_wakee);
+    create_thread(&wakee_tid, &wakee_attr, wakee_fn, "wakee");
+
+    set_thread_affinity(&waker_attr, cpuset_waker);
+    create_thread(&waker_tid, &waker_attr, waker_fn, "waker");
+
+    CPU_FREE(cpuset_wakee);
+    CPU_FREE(cpuset_waker);
+}
+
+void cleanup(void) {
+    pthread_join(wakee_tid, NULL);
+    pthread_join(waker_tid, NULL);
+}
+
+
 void print_usage(void) {
     printf("Usage: [options]\n"
            "-w <wakee_cpu>          CPU to run wakee thread on\n"
@@ -112,11 +193,9 @@ void print_usage(void) {
            "-h                      Show this help message\n");
 }
 
-// Main function
 int main(int argc, char *argv[]) {
     int opt;
 
-    // Argument parsing
     while ((opt = getopt(argc, argv, "w:e:s:d:pth")) != -1) {
         switch (opt) {
         case 'w':
@@ -153,7 +232,12 @@ int main(int argc, char *argv[]) {
     printf("Total CPU Idle states: %d\n", total_idle_states);
 
     initialize_wakee_idle_states();
+    initialize_threads();
 
+    sleep(test_duration_sec);
+    stop = 1;
+
+    cleanup();
     return 0;
 }
 
